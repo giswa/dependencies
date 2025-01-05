61c0807cde90213ff4153aa02e0cb5e419f82c1b
Gislain Wautriche <gislain.wautriche@pharmatic.ch>
2025-01-05 20:11:18 +0100

    Added depth information

--- Project.cs
+++ Project.cs
@@ -7,16 +7,21 @@ public class Project{
     private XmlDocument XmlTree { get; set; }
     private string BaseDirectory { get; set; }
     private string Filename { get; set; }
+    private int Depth { get; set; }
     public List<Project> Dependencies {get; set;}
 
-    public Project(string path){
+    public Project(string path) : this(path, 0) {}
+
+    public Project(string path, int depth) 
+    {
+        Depth = depth + 1 ;
         SourcePath = path ;
         BaseDirectory = new FileInfo(path).Directory.FullName;
-        Filename = Path.GetFileName(path);
-        
+        Filename = Path.GetFileName(path);  
+    
         // recursive search sub-dependencies
         Search();
-        Console.WriteLine($"Parsed: {Filename}. Found {Dependencies.Count} dependencies");  
+        Console.WriteLine($"Parsed: {SourcePath}. Depth {Depth}, Found {Dependencies.Count} dependencies");  
     }
 
     public void Search(){
@@ -35,7 +40,7 @@ public class Project{
                 // replace DirectorySeparatorChar. Seems to be always in Windows format in .proj files
                 referencePath = referencePath.Replace(@"\", Path.DirectorySeparatorChar.ToString());
                 string fullPath = Path.GetFullPath(referencePath, BaseDirectory);
-                Project p = new Project(fullPath);
+                Project p = new Project(fullPath, Depth);
                 // add to dependency
                 Dependencies.Add(p);
 
